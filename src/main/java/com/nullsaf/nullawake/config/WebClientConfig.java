160b3657992d48474a63c8f9fd3ed1ee4a60f150
kingwhangzang <soyun011112@sookmyung.ac.kr>
2026-05-13 16:15:27 +0900

    Fix : import문 삭제

--- src/main/java/com/nullsaf/nullawake/config/WebClientConfig.java
+++ src/main/java/com/nullsaf/nullawake/config/WebClientConfig.java
@@ -1,13 +1,9 @@
 package com.nullsaf.nullawake.config;
 
-import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
-import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
