f5cb66c4e6df0de90c37360727eaa279f427c617
kingwhangzang <soyun011112@sookmyung.ac.kr>
2026-05-13 15:15:06 +0900

    Fix : 로그인 시도를 위한 timeout 설정

--- src/main/java/com/nullsaf/nullawake/config/WebClientConfig.java
+++ src/main/java/com/nullsaf/nullawake/config/WebClientConfig.java
@@ -1,15 +1,25 @@
 package com.nullsaf.nullawake.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofSeconds(5));
+
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
