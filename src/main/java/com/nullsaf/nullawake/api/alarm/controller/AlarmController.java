fa519db895a3097602350bfbbde98321a1dc2acf
kingwhangzang <soyun011112@sookmyung.ac.kr>
2026-05-18 11:36:23 +0900

    Feat : 알람 CRUD 및 조회 API 구현

---
+++ src/main/java/com/nullsaf/nullawake/api/alarm/controller/AlarmController.java
@@ -0,0 +1,122 @@
+package com.nullsaf.nullawake.api.alarm.controller;
+
+import com.nullsaf.nullawake.api.alarm.dto.request.AlarmRequest;
+import com.nullsaf.nullawake.api.alarm.dto.request.AlarmSelectedRequest;
+import com.nullsaf.nullawake.api.alarm.dto.response.AlarmCategoryStackResponse;
+import com.nullsaf.nullawake.api.alarm.dto.response.AlarmResponse;
+import com.nullsaf.nullawake.api.alarm.service.AlarmCommandService;
+import com.nullsaf.nullawake.api.alarm.service.AlarmQueryService;
+import com.nullsaf.nullawake.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@Tag(name = "Alarm", description = "알람 API")
+@SecurityRequirement(name = "bearerAuth")
+@RestController
+@RequestMapping("/api/alarms")
+@RequiredArgsConstructor
+public class AlarmController {
+
+    private final AlarmQueryService alarmQueryService;
+    private final AlarmCommandService alarmCommandService;
+
+    @Operation(summary = "알람 목록 조회")
+    @GetMapping
+    public ApiResponse<AlarmResponse.ListResponse> getAlarms() {
+        Long userId = 11L;
+
+        return ApiResponse.success(
+                "알람 목록 조회에 성공했습니다",
+                alarmQueryService.getAlarms(userId)
+        );
+    }
+
+    @Operation(summary = "알람 생성")
+    @PostMapping
+    public ApiResponse<AlarmResponse.CreateResponse> createAlarm(
+            @Valid @RequestBody AlarmRequest request
+    ) {
+        Long userId = 11L;
+
+        Long alarmId = alarmCommandService.createAlarm(userId, request);
+
+        return ApiResponse.created(
+                "알람 생성에 성공했습니다.",
+                new AlarmResponse.CreateResponse(alarmId)
+        );
+    }
+
+    @Operation(summary = "알람 상세 조회")
+    @GetMapping("/{alarmGroupId}")
+    public ApiResponse<AlarmResponse.DetailResponse> getAlarmDetail(
+            @PathVariable Long alarmGroupId
+    ) {
+        Long userId = 11L;
+
+        return ApiResponse.success(
+                "알람 조회에 성공했습니다.",
+                alarmQueryService.getAlarmDetail(userId, alarmGroupId)
+        );
+    }
+
+    @Operation(summary = "알람 수정")
+    @PutMapping("/{alarmGroupId}")
+    public ApiResponse<Void> updateAlarm(
+            @PathVariable Long alarmGroupId,
+            @Valid @RequestBody AlarmRequest request
+    ) {
+        Long userId = 11L;
+
+        alarmCommandService.updateAlarm(userId, alarmGroupId, request);
+
+        return ApiResponse.success("알람 수정에 성공했습니다.");
+    }
+
+    @Operation(summary = "알람 삭제")
+    @DeleteMapping("/{alarmGroupId}")
+    public ApiResponse<Void> deleteAlarm(
+            @PathVariable Long alarmGroupId
+    ) {
+        Long userId = 11L;
+
+        alarmCommandService.deleteAlarm(userId, alarmGroupId);
+
+        return ApiResponse.success("알람 삭제에 성공했습니다.");
+    }
+
+    @Operation(summary = "알람 활성화/비활성화")
+    @PatchMapping("/{alarmGroupId}/selected")
+    public ApiResponse<AlarmResponse.SelectedResponse> updateSelected(
+            @PathVariable Long alarmGroupId,
+            @Valid @RequestBody AlarmSelectedRequest request
+    ) {
+        Long userId = 11L;
+
+        Boolean selected = alarmCommandService.updateSelected(userId, alarmGroupId, request);
+
+        return ApiResponse.success(
+                "알람 상태 변경에 성공했습니다.",
+                new AlarmResponse.SelectedResponse(alarmGroupId, selected)
+        );
+    }
+
+    @Operation(summary = "알람 생성용 카테고리/스택 조회")
+    @GetMapping("/category_stacks")
+    public ApiResponse<AlarmCategoryStackResponse> getCategoryStacks() {
+
+        Long userId = 11L;
+
+        AlarmCategoryStackResponse response =
+                alarmQueryService.getCategoryStacks(userId);
+
+        return ApiResponse.success(
+                "활성화된 카테고리/스택 조회에 성공했습니다.",
+                response
+        );
+    }
+}
