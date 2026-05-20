46a4ad4f27a8d65e4ec99302ea36954f9e2b75fa
kingwhangzang <soyun011112@sookmyung.ac.kr>
2026-05-20 13:42:34 +0900

    Add : 주석 작성, 메서드 통합

--- src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionChoiceRepository.java
+++ src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionChoiceRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * 객관식 문제의 선지 조회
+ */
 public interface QuestionChoiceRepository extends JpaRepository<QuestionChoice, Long> {
 
+    // 정답인 선지 조회
     Optional<QuestionChoice> findByQuestionQuestionIdAndIsAnswerTrue(Long questionId);
 
+    // 문제 풀이 제출 시, 사용자가 제출한 선지가 해당 문제에 속하는지 검증
     Optional<QuestionChoice> findByChoiceIdAndQuestionQuestionId(Long choiceId, Long questionId);
 }
--- src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionHistoryRepository.java
+++ src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionHistoryRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * 사용자의 문제 풀이 기록 조회
+ */
 public interface QuestionHistoryRepository extends JpaRepository<QuestionHistory, Long> {
 
+    // 풀이 기록 하나 조회. 이때 Question 정보도 같이 가져온다
     @EntityGraph(attributePaths = {"question"})
     Optional<QuestionHistory> findByHistoryId(Long historyId);
-
-    default Optional<QuestionHistory> findByIdWithQuestion(Long historyId) {
-        return findByHistoryId(historyId);
-    }
 }
