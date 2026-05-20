0f8d5a6dc70419c523dad6123b3b6aefca1dda05
kingwhangzang <soyun011112@sookmyung.ac.kr>
2026-05-20 13:42:17 +0900

    Add : 주석 작성

--- src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionRepository.java
+++ src/main/java/com/nullsaf/nullawake/api/question/repository/QuestionRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * 문제 테이블 조회
+ */
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    /**
+     * 문제 한 개 조회
+     * @param questionId questionId
+     * @return 문제 하나의 정보, 이때 techStack, choice(객관식 보기)도 함께 조회
+     */
     @EntityGraph(attributePaths = {"techStack", "choices"})
     Optional<Question> findByQuestionId(Long questionId);
-
-    default Optional<Question> findByIdWithTechStack(Long questionId) {
-        return findByQuestionId(questionId);
-    }
 }
